6f50cd685154a165eb75b043c29fc34f831881aa
mischadab <mischadabrowski@gmail.com>
2021-02-17 16:19:40 -0500

    enable express

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-// require express
+// require dependencies
 const express = require('express');
-const app = express()
+const fs = require('fs');
+
+// enable express
+const app = express();
+app.use( express.static( 'html' ) )
+
+// read post data
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
