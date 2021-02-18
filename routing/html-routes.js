df4f1816b62010d08345b8f3f4214695bf55b4c3
mischadab <mischadabrowski@gmail.com>
2021-02-18 15:55:00 -0500

    fixed get again

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 // get requests to the html
-app.get('/', function(req, res){
+app.get('*', function(req, res){
     res.SendFile(path.join(__dirname + '/../public/index.html'))
 })
 
-// all other routes into the notes html
-app.get('*', function(req, res){
+// get notes into the notes html
+app.get('/notes', function(req, res){
     res.SendFile(path.join(__dirname + '/../public/notes.html'))
 })
