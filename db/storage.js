f37b15e0bacb3cf5e0e4868cb78f9e89f2c7ce58
mischadab <mischadabrowski@gmail.com>
2021-02-18 16:02:08 -0500

    read/write file sync

--- db/storage.js
+++ db/storage.js
@@ -3,4 +3,8 @@ const fs = require('fs')
 // npm package to create unique ids in Express.js
 const uuid = require('uuid')
 
-// use of utilities in node
+// use of utilities in node to create promisified functions to read and write files with fs
+const readFile = util.promisify(fs.readFileSync);
+const writeFile = util.promisify(fs.writeFileSync);
+
+// create class to hold CRUD
