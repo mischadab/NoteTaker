56d08bc951b5566f3834b00cd00deaaa9d489e65
mischadab <mischadabrowski@gmail.com>
2021-02-20 20:05:38 -0500

    enable express

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-// requirements
+// require dependencies 
 const express = require('express');
+const fs = require('fs');
+
+// enable express
 const app = express();
+app.use( express.static( 'html') )
+
+// port
+const PORT = process.env.PORT || 8080;
